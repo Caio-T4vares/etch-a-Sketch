12c97e4efeac8092c3a55eb0d41329e4adbcdbac
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-10 13:26:30 -0300

    Add function to remove square background

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 const board = document.querySelector("#board");
 const sizeInput = document.querySelector("#sizeInput");
 const sizeLabel = document.querySelector(".input-container label");
+const clearButton = document.querySelector("#clear-button");
 
 generateBoard(16);
+
+clearButton.addEventListener("click", () => {
+  clearBoard();
+});
 
 sizeInput.addEventListener("input", (e) => {
   const size = e.target.value;
   sizeLabel.textContent = `${size}x${size}`;
 
-  clearBoard();
+  removeBoard();
   generateBoard(size);
 });
 
+function clearBoard() {
+  let lines = board.children;
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    for (let j = 0; j < line.children.length; j++) {
+      line.children[j].style.backgroundColor = "";
+    }
+  }
+}
+
 function generateBoard(size) {
   for (let i = 0; i < size; i++) {
-    const newColumn = document.createElement("div");
-    newColumn.classList.add("column");
+    const newLine = document.createElement("div");
+    newLine.classList.add("line");
 
     for (let j = 0; j < size; j++) {
       const newSquare = document.createElement("div");
       newSquare.classList.add("square");
 
-      newColumn.append(newSquare);
+      newLine.append(newSquare);
 
       newSquare.addEventListener("mouseenter", (e) => {
         e.target.style.backgroundColor = "black";
       });
     }
 
-    board.append(newColumn);
+    board.append(newLine);
   }
 }
 
-function clearBoard() {
+function removeBoard() {
   let child;
   while ((child = board.firstChild)) {
     child.remove();
