b21693638bbb4bdea78592bf57d213ba0e1a0ad4
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-10 11:18:52 -0300

    Add a input to change board size

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const board = document.querySelector("#board");
+const sizeInput = document.querySelector("#sizeInput");
+const sizeLabel = document.querySelector(".input-container label");
 
 generateBoard(16);
+
+sizeInput.addEventListener("input", (e) => {
+  const size = e.target.value;
+  sizeLabel.textContent = `${size}x${size}`;
+
+  generateBoard(size);
+});
 
 function generateBoard(size) {
   for (let i = 0; i < size; i++) {
